8579aab4858bb390db8b6bc8040b935a0329c62f
Gwen Lofman <Gwen@Lofman.io>
2020-02-25 22:15:55 -0500

    Set window title to immediate-mode

--- examples/glium/main.rs
+++ examples/glium/main.rs
@@ -130,7 +130,7 @@ fn colors(bg: Color, fg: &[Color], x1: f32, x2: f32, y1: f32, y2: f32) -> (Vec<V
 
 fn main() {
     let event_loop = glutin::event_loop::EventLoop::new();
-    let wb = glutin::window::WindowBuilder::new();
+    let wb = glutin::window::WindowBuilder::new().with_title("immediate-mode");
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
