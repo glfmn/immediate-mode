4c9f5e21c63048cca4407731ce6b1b02ac1b2ae8
Gwen Lofman <Gwen@Lofman.io>
2020-02-27 18:10:03 -0500

    Remove default test

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,6 @@
 //! # immediate-mode
 //!
 //! 2D immediate mode user interface for Rust
+
 pub mod color;
 pub mod math;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
